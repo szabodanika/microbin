36fa6598a837378dfce6d81175f2b8d6fbf90bd5
Daniel Szabo <daniel.szabo99@outlook.com>
2022-05-02 16:53:10 +0100

    File upload and persistence extension - index.html extended with form input - pasta.html and pastalist.html show link to /file/{pasta.id}/{filename} path - files are saved in pasta_data folder - all data is now stored in pasta_data/database.json - changed pastalist.html date format to exclude year - added custom 404 error handler

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 extern crate core;
 
-use actix_files as fs;
-use actix_web::web::Data;
-use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
-use askama::Template;
-use clap::Parser;
-use linkify::{LinkFinder, LinkKind};
-use rand::Rng;
-use std::path::PathBuf;
+use env_logger::Builder;
+use std::io::Write;
 use std::sync::Mutex;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use actix_files as fs;
+use actix_multipart::Multipart;
+use actix_web::{get, web, App, Error, HttpResponse, HttpServer, Responder};
+use askama::Template;
+use chrono::Local;
+use clap::Parser;
+use futures::TryStreamExt as _;
+use linkify::{LinkFinder, LinkKind};
+use log::LevelFilter;
+use rand::Rng;
+
 use crate::animalnumbers::{to_animal_names, to_u64};
-use crate::pasta::{Pasta, PastaFormData};
+use crate::dbio::save_to_file;
+use crate::pasta::Pasta;
 
 mod animalnumbers;
+mod dbio;
 mod pasta;
 
 struct AppState {
@@ -31,6 +38,10 @@ struct Args {
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate {}
+
+#[derive(Template)]
+#[template(path = "error.html")]
+struct ErrorTemplate {}
 
 #[derive(Template)]
 #[template(path = "pasta.html")]
@@ -51,48 +62,94 @@ async fn index() -> impl Responder {
         .body(IndexTemplate {}.render().unwrap())
 }
 
-#[post("/create")]
-async fn create(data: web::Data<AppState>, pasta: web::Form<PastaFormData>) -> impl Responder {
-    let mut pastas = data.pastas.lock().unwrap();
+async fn not_found() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Found()
+        .content_type("text/html")
+        .body(ErrorTemplate {}.render().unwrap()))
+}
 
-    let inner_pasta = pasta.into_inner();
+async fn create(data: web::Data<AppState>, mut payload: Multipart) -> Result<HttpResponse, Error> {
+    let mut pastas = data.pastas.lock().unwrap();
 
     let timenow: i64 = match SystemTime::now().duration_since(UNIX_EPOCH) {
         Ok(n) => n.as_secs(),
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     } as i64;
 
-    let expiration = match inner_pasta.expiration.as_str() {
-        "1min" => timenow + 60,
-        "10min" => timenow + 60 * 10,
-        "1hour" => timenow + 60 * 60,
-        "24hour" => timenow + 60 * 60 * 24,
-        "1week" => timenow + 60 * 60 * 24 * 7,
-        "never" => 0,
-        _ => panic!("Unexpected expiration time!"),
-    };
-
-    let pasta_type = if is_valid_url(inner_pasta.content.as_str()) {
-        String::from("url")
-    } else {
-        String::from("text")
-    };
-
-    let new_pasta = Pasta {
+    let mut new_pasta = Pasta {
         id: rand::thread_rng().gen::<u16>() as u64,
-        content: inner_pasta.content,
+        content: String::from("No Text Content"),
+        file: String::from("no-file"),
         created: timenow,
-        pasta_type,
-        expiration,
+        pasta_type: String::from(""),
+        expiration: 0,
     };
+
+    while let Some(mut field) = payload.try_next().await? {
+        match field.name() {
+            "expiration" => {
+                while let Some(chunk) = field.try_next().await? {
+                    new_pasta.expiration = match std::str::from_utf8(&chunk).unwrap() {
+                        "1min" => timenow + 60,
+                        "10min" => timenow + 60 * 10,
+                        "1hour" => timenow + 60 * 60,
+                        "24hour" => timenow + 60 * 60 * 24,
+                        "1week" => timenow + 60 * 60 * 24 * 7,
+                        "never" => 0,
+                        _ => panic!("Unexpected expiration time!"),
+                    };
+                }
+
+                continue;
+            }
+            "content" => {
+                while let Some(chunk) = field.try_next().await? {
+                    new_pasta.content = std::str::from_utf8(&chunk).unwrap().to_string();
+                    new_pasta.pasta_type = if is_valid_url(new_pasta.content.as_str()) {
+                        String::from("url")
+                    } else {
+                        String::from("text")
+                    };
+                }
+                continue;
+            }
+            "file" => {
+                let content_disposition = field.content_disposition();
+
+                let filename = match content_disposition.get_filename() {
+                    Some("") => continue,
+                    Some(filename) => filename.replace(' ', "_").to_string(),
+                    None => continue,
+                };
+
+                std::fs::create_dir_all(format!("./pasta_data/{}", &new_pasta.id_as_animals()))
+                    .unwrap();
+
+                let filepath = format!("./pasta_data/{}/{}", &new_pasta.id_as_animals(), &filename);
+
+                new_pasta.file = filename;
+
+                let mut f = web::block(|| std::fs::File::create(filepath)).await??;
+
+                while let Some(chunk) = field.try_next().await? {
+                    f = web::block(move || f.write_all(&chunk).map(|_| f)).await??;
+                }
+
+                new_pasta.pasta_type = String::from("text");
+            }
+            _ => {}
+        }
+    }
 
     let id = new_pasta.id;
 
     pastas.push(new_pasta);
 
-    HttpResponse::Found()
+    save_to_file(&pastas);
+
+    Ok(HttpResponse::Found()
         .append_header(("Location", format!("/pasta/{}", to_animal_names(id))))
-        .finish()
+        .finish())
 }
 
 #[get("/pasta/{id}")]
@@ -110,7 +167,9 @@ async fn getpasta(data: web::Data<AppState>, id: web::Path<String>) -> HttpRespo
         }
     }
 
-    HttpResponse::Found().body("Pasta not found! :-(")
+    HttpResponse::Found()
+        .content_type("text/html")
+        .body(ErrorTemplate {}.render().unwrap())
 }
 
 #[get("/url/{id}")]
@@ -138,6 +197,7 @@ async fn redirecturl(data: web::Data<AppState>, id: web::Path<String>) -> HttpRe
 #[get("/raw/{id}")]
 async fn getrawpasta(data: web::Data<AppState>, id: web::Path<String>) -> String {
     let mut pastas = data.pastas.lock().unwrap();
+
     let id = to_u64(&*id.into_inner());
 
     remove_expired(&mut pastas);
@@ -166,7 +226,6 @@ async fn remove(data: web::Data<AppState>, id: web::Path<String>) -> HttpRespons
                 .finish();
         }
     }
-
     HttpResponse::Found().body("Pasta not found! :-(")
 }
 
@@ -184,26 +243,44 @@ async fn list(data: web::Data<AppState>) -> HttpResponse {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    println!(
-        "{}",
-        format!("Listening on http://127.0.0.1:{}", args.port.to_string())
+
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] - {}",
+                Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter(None, LevelFilter::Info)
+        .init();
+
+    log::info!(
+        "MicroBin listening on http://127.0.0.1:{}",
+        args.port.to_string()
     );
 
+    std::fs::create_dir_all("./pasta_data").unwrap();
+
     let data = web::Data::new(AppState {
-        pastas: Mutex::new(Vec::new()),
+        pastas: Mutex::new(dbio::load_from_file().unwrap()),
     });
 
     HttpServer::new(move || {
         App::new()
             .app_data(data.clone())
             .service(index)
-            .service(create)
             .service(getpasta)
             .service(redirecturl)
             .service(getrawpasta)
             .service(remove)
             .service(list)
             .service(fs::Files::new("/static", "./static"))
+            .service(fs::Files::new("/file", "./pasta_data"))
+            .service(web::resource("/upload").route(web::post().to(create)))
+            .default_service(web::route().to(not_found))
     })
     .bind(format!("127.0.0.1:{}", args.port.to_string()))?
     .run()
--- src/pasta.rs
+++ src/pasta.rs
@@ -1,58 +1,51 @@
 use std::fmt;
-use actix_web::cookie::time::macros::format_description;
-use chrono::{Datelike, DateTime, NaiveDateTime, Timelike, Utc};
-use serde::Deserialize;
+
+use chrono::{DateTime, Datelike, NaiveDateTime, Timelike, Utc};
+use serde::{Deserialize, Serialize};
+
 use crate::to_animal_names;
 
+#[derive(Serialize, Deserialize)]
 pub struct Pasta {
-	pub id: u64,
-	pub content: String,
-	pub created: i64,
-	pub expiration: i64,
-	pub pasta_type: String
-}
-
-#[derive(Deserialize)]
-pub struct PastaFormData {
-	pub content: String,
-	pub expiration: String
+    pub id: u64,
+    pub content: String,
+    pub file: String,
+    pub created: i64,
+    pub expiration: i64,
+    pub pasta_type: String,
 }
 
 impl Pasta {
+    pub fn id_as_animals(&self) -> String {
+        to_animal_names(self.id)
+    }
 
-	pub fn idAsAnimals(&self) -> String {
-		to_animal_names(self.id)
-	}
+    pub fn created_as_string(&self) -> String {
+        let date = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(self.created, 0), Utc);
+        format!(
+            "{:02}-{:02} {}:{}",
+            date.month(),
+            date.day(),
+            date.hour(),
+            date.minute(),
+        )
+    }
 
-	pub fn createdAsString(&self) -> String {
-		let date = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(self.created, 0), Utc);
-		format!(
-			"{}-{:02}-{:02} {}:{}",
-			date.year(),
-			date.month(),
-			date.day(),
-			date.hour(),
-			date.minute(),
-		)
-	}
-
-	pub fn expirationAsString(&self) -> String {
-		let date = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(self.expiration, 0), Utc);
-		format!(
-			"{}-{:02}-{:02} {}:{}",
-			date.year(),
-			date.month(),
-			date.day(),
-			date.hour(),
-			date.minute(),
-		)
-	}
-
+    pub fn expiration_as_string(&self) -> String {
+        let date =
+            DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(self.expiration, 0), Utc);
+        format!(
+            "{:02}-{:02} {}:{}",
+            date.month(),
+            date.day(),
+            date.hour(),
+            date.minute(),
+        )
+    }
 }
 
-
 impl fmt::Display for Pasta {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		write!(f, "{}", self.content)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.content)
+    }
 }
